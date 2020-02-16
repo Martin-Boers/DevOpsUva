cb3d4adea444a0d9b2ab5211a5e4faebccacd057
Martin Boers <martin-003@live.nl>
2020-02-16 22:04:14 +0100

    Changed yaml to use grades as simple string instead of Grade object due to import issues in the Python code.

--- python-flask-server/swagger_server/models/__init__.py
+++ python-flask-server/swagger_server/models/__init__.py
@@ -3,5 +3,4 @@
 # flake8: noqa
 from __future__ import absolute_import
 # import models into model package
-from swagger_server.models.grade import Grade
 from swagger_server.models.student import Student
--- python-flask-server/swagger_server/models/student.py
+++ python-flask-server/swagger_server/models/student.py
@@ -15,7 +15,7 @@ class Student(Model):
     Do not edit the class manually.
     """
 
-    def __init__(self, student_id: int=None, first_name: str=None, last_name: str=None, grades: Dict[str, Grade]=None):  # noqa: E501
+    def __init__(self, student_id: int=None, first_name: str=None, last_name: str=None, grades: Dict[str, str]=None):  # noqa: E501
         """Student - a model defined in Swagger
 
         :param student_id: The student_id of this Student.  # noqa: E501
@@ -25,13 +25,13 @@ class Student(Model):
         :param last_name: The last_name of this Student.  # noqa: E501
         :type last_name: str
         :param grades: The grades of this Student.  # noqa: E501
-        :type grades: Dict[str, Grade]
+        :type grades: Dict[str, str]
         """
         self.swagger_types = {
             'student_id': int,
             'first_name': str,
             'last_name': str,
-            'grades': Dict[str, Grade]
+            'grades': Dict[str, str]
         }
 
         self.attribute_map = {
@@ -121,22 +121,22 @@ class Student(Model):
         self._last_name = last_name
 
     @property
-    def grades(self) -> Dict[str, Grade]:
+    def grades(self) -> Dict[str, str]:
         """Gets the grades of this Student.
 
 
         :return: The grades of this Student.
-        :rtype: Dict[str, Grade]
+        :rtype: Dict[str, str]
         """
         return self._grades
 
     @grades.setter
-    def grades(self, grades: Dict[str, Grade]):
+    def grades(self, grades: Dict[str, str]):
         """Sets the grades of this Student.
 
 
         :param grades: The grades of this Student.
-        :type grades: Dict[str, Grade]
+        :type grades: Dict[str, str]
         """
 
         self._grades = grades
